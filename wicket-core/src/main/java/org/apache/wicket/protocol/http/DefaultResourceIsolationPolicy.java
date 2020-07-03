4b804b30c79bfeabcf82a52e4dc281806570dc40
Ecenaz Jen Ozmen <eo2419@columbia.edu>
2020-07-03 16:02:29 +0000

    Added javadoc explanation + reformatting

--- wicket-core/src/main/java/org/apache/wicket/protocol/http/DefaultResourceIsolationPolicy.java
+++ wicket-core/src/main/java/org/apache/wicket/protocol/http/DefaultResourceIsolationPolicy.java
@@ -1,36 +1,43 @@
 package org.apache.wicket.protocol.http;
 
-import org.apache.wicket.protocol.http.ResourceIsolationPolicy;
-
 import javax.servlet.http.HttpServletRequest;
 
-public class DefaultResourceIsolationPolicy implements ResourceIsolationPolicy {
+/**
+ * Default resource isolation policy used in {@link CsrfPreventionRequestCycleListener}
+ * that implements the {@link ResourceIsolationPolicy} interface. This default
+ * policy is based on <a href="https://web.dev/fetch-metadata/">https://web.dev/fetch-metadata/</a>.
+ *
+ * @see <a href="https://web.dev/fetch-metadata/">https://web.dev/fetch-metadata/</a>
+ *
+ * @author Santiago Diaz
+ * @author Ecenaz Jen Ozmen
+ */
+public class DefaultResourceIsolationPolicy implements ResourceIsolationPolicy
+{
 
-  @Override
-  public boolean isRequestAllowed(HttpServletRequest request) {
-    String site = request.getHeader(SEC_FETCH_SITE_HEADER);
+    @Override
+    public boolean isRequestAllowed(HttpServletRequest request)
+    {
+        String site = request.getHeader(SEC_FETCH_SITE_HEADER);
 
-    // TODO: Is this needed?
-    site = site.toLowerCase();
+        // Allow same-site and browser-initiated requests
+        if (SAME_ORIGIN.equals(site) || SAME_SITE.equals(site) || NONE.equals(site)) {
+            return true;
+        }
 
-    // Allow same-site and browser-initiated requests
-    if (SAME_ORIGIN.equals(site) || SAME_SITE.equals(site) || NONE.equals(site)) {
-      return true;
+        // Allow simple top-level navigations except <object> and <embed>
+        return isAllowedTopLevelNavigation(request);
     }
 
-    // Allow simple top-level navigations except <object> and <embed>
-    return isAllowedTopLevelNavigation(request);
-  }
+    private boolean isAllowedTopLevelNavigation(HttpServletRequest request)
+    {
+        String mode = request.getHeader(SEC_FETCH_MODE_HEADER);
+        String dest = request.getHeader(SEC_FETCH_DEST_HEADER);
 
-  private boolean isAllowedTopLevelNavigation(HttpServletRequest request) {
-    // TODO: Assert these headers are never null at this point?
-    String mode = request.getHeader(SEC_FETCH_MODE_HEADER);
-    String dest = request.getHeader(SEC_FETCH_DEST_HEADER);
+        // TODO: Does the method need to be upper cased?
+        boolean isSimpleTopLevelNavigation = MODE_NAVIGATE.equals(mode) || "GET".equals(request.getMethod());
+        boolean isNotObjectOrEmbedRequest = !DEST_EMBED.equals(dest) && !DEST_OBJECT.equals(dest);
 
-    // TODO: Does the method need to be upper cased?
-    boolean isSimpleTopLevelNavigation = MODE_NAVIGATE.equals(mode) || "GET".equals(request.getMethod());
-    boolean isNotObjectOrEmbedRequest = !DEST_EMBED.equals(dest) && !DEST_OBJECT.equals(dest);
-
-    return isSimpleTopLevelNavigation && isNotObjectOrEmbedRequest;
-  }
+        return isSimpleTopLevelNavigation && isNotObjectOrEmbedRequest;
+    }
 }
--- wicket-core/src/main/java/org/apache/wicket/protocol/http/ResourceIsolationPolicy.java
+++ wicket-core/src/main/java/org/apache/wicket/protocol/http/ResourceIsolationPolicy.java
@@ -2,22 +2,39 @@ package org.apache.wicket.protocol.http;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Interface for the resource isolation policies to be used for
+ * fetch metadata checks.
+ *
+ * Resource isolation policies are designed to protect against cross
+ * origin attacks and use the {@code sec-fetch-*} request headers to decide
+ * whether to accept or reject a request. Read more about
+ * <a href="https://web.dev/fetch-metadata/">Fetch Metadata.</a>
+ *
+ * See {@link DefaultResourceIsolationPolicy} for the default implementation used.
+ *
+ * @see <a href="https://web.dev/fetch-metadata/">https://web.dev/fetch-metadata/</a>
+ *
+ * @author Santiago Diaz
+ * @author Ecenaz Jen Ozmen
+ */
 @FunctionalInterface
-public interface ResourceIsolationPolicy {
-  String SEC_FETCH_SITE_HEADER = "sec-fetch-site";
-  String SEC_FETCH_MODE_HEADER = "sec-fetch-mode";
-  String SEC_FETCH_DEST_HEADER = "sec-fetch-dest";
-  String SAME_ORIGIN = "same-origin";
-  String SAME_SITE = "same-site";
-  String NONE = "none";
-  String MODE_NAVIGATE = "navigate";
-  String DEST_OBJECT = "object";
-  String DEST_EMBED = "embed";
-  // TODO: Add remaining Fetch Metadata keywords
-  String CROSS_SITE = "cross-site";
-  String CORS = "cors";
-  String DEST_SCRIPT = "script";
-  String DEST_IMAGE = "image";
+public interface ResourceIsolationPolicy
+{
+    String SEC_FETCH_SITE_HEADER = "sec-fetch-site";
+    String SEC_FETCH_MODE_HEADER = "sec-fetch-mode";
+    String SEC_FETCH_DEST_HEADER = "sec-fetch-dest";
+    String SAME_ORIGIN = "same-origin";
+    String SAME_SITE = "same-site";
+    String NONE = "none";
+    String MODE_NAVIGATE = "navigate";
+    String DEST_OBJECT = "object";
+    String DEST_EMBED = "embed";
+    // TODO: Add remaining Fetch Metadata keywords
+    String CROSS_SITE = "cross-site";
+    String CORS = "cors";
+    String DEST_SCRIPT = "script";
+    String DEST_IMAGE = "image";
 
-  boolean isRequestAllowed(HttpServletRequest request);
+    boolean isRequestAllowed(HttpServletRequest request);
 }
